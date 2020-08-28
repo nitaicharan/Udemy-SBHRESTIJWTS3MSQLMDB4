ad46db44da6824a39430c880e4428deee762d656
Nitai Charan <nitaicharan@gmail.com>
2020-08-27 22:08:35 -0300

    119. Criando um interceptor para tratamento de erros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpErrorInterceptor } from 'src/interceptors/error-interceptor';
 import { CategoriaService } from './../services/domain/categoria.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +24,8 @@ import { AppComponent } from './app.component';
     StatusBar,
     SplashScreen,
     CategoriaService,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/interceptors/error-interceptor.ts
@@ -0,0 +1,18 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+export class HttpErrorInterceptor implements HttpInterceptor {
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        return next.handle(request).pipe(
+            retry(1),
+            catchError((error: HttpErrorResponse) => {
+                const errorObj = error.error ?? error;
+
+                console.log('Erro detectado pelo interceptor:', errorObj);
+                return throwError(errorObj);
+            })
+        );
+    }
+}

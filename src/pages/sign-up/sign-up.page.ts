c8160186d6e8216605b240d433a1d47d28ce385b
Nitai Charan <nitaicharan@gmail.com>
2020-09-12 00:00:00 -0300

    132. Criando um FormGroup para controlar o formulário

--- src/pages/sign-up/sign-up.page.ts
+++ src/pages/sign-up/sign-up.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,6 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.page.scss'],
 })
 export class SignUpPage {
+  formGroup: FormGroup;
+
+  constructor(
+    formBuilder: FormBuilder
+  ) {
+
+    this.formGroup = formBuilder.group({
+      nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
+      tipo: ['1', [Validators.required]],
+      cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['123', [Validators.required]],
+      logradouro: ['Rua Via', [Validators.required]],
+      numero: ['25', [Validators.required]],
+      complemento: ['Apto 3', []],
+      bairro: ['Copacabana', []],
+      cep: ['10828333', [Validators.required]],
+      telefone1: ['977261827', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]]
+    });
+  }
+
   signupUser() {
     console.log("enviou o form");
   }

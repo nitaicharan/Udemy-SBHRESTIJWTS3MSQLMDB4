a2b669e666359ddd34b2a2b972bb9b3b8b077b80
Nitai Charan <nitaicharan@gmail.com>
2020-09-21 20:39:15 -0300

    148. Armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.page.ts
+++ src/pages/pick-address/pick-address.page.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { ClienteDTO } from 'src/models/cliente.dto';
 import { ClienteService } from 'src/services/domain/cliente.service';
 import { EnderecoDTO } from './../../models/endereco.dto';
+import { PedidoDTO } from './../../models/pedido.dto';
+import { CartService } from './../../services/cart.service';
 import { StorageService } from './../../services/storage.service';
 
 @Component({
@@ -14,9 +16,11 @@ import { StorageService } from './../../services/storage.service';
 })
 export class PickAddressPage {
   items$: Observable<EnderecoDTO[]>;
+  pedido: PedidoDTO;
 
   constructor(
     router: Router,
+    cartService: CartService,
     storageService: StorageService,
     clienteService: ClienteService,
   ) {
@@ -24,10 +28,24 @@ export class PickAddressPage {
     const localUser = storageService.getLocalUser();
     if (localUser && localUser.email) {
       this.items$ = clienteService.findByEmail(localUser.email).pipe(
+        tap((cliente: ClienteDTO) => {
+          const cart = cartService.getCart();
+          this.pedido = {
+            pagamento: null,
+            enderecoDeEntrega: null,
+            cliente: { id: cliente['id'] },
+            itens: cart.items.map(i => ({ quantidade: i.quantidade, produto: { id: i.produto.id } }))
+          };
+        }),
         map((cliente: ClienteDTO) => cliente['enderecos']),
         take(1),
       );
     }
     else router.navigateByUrl('/');
+  }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = { id: item.id };
+    console.log(this.pedido);
   }
 }
